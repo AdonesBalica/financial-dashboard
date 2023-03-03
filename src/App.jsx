b8c31ff5f7938c6abee8229a558415a67c7e0302
Adones Balica <adonesbalica@gmail.com>
2023-03-03 20:45:45 -0300

    ➕Adicionando styled-components e um globalStyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css';
+import GlobalStyle from './globalStyles';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <GlobalStyle />
+      <Home />
     </div>
   );
 }
